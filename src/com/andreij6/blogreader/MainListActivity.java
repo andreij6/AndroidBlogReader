b66df6d4bb68cc8b2ccff9cdd0c2406bce0783f2
Andre Jones <andreij6@gmail.com>
2013-10-27 17:36:52 -0500

    Getting JSON Data from an HTTP Request

--- src/com/andreij6/blogreader/MainListActivity.java
+++ src/com/andreij6/blogreader/MainListActivity.java
@@ -1,12 +1,22 @@
 package com.andreij6.blogreader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 	
@@ -20,15 +30,27 @@ public class MainListActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 		
-		try {
-			URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+		if(isNetworkAvailable()){
+			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+			getBlogPostsTask.execute();
 		}
-		catch(MalformedURLException e) {
-			Log.e(TAG, "Exception caught: ", e);
+		else {
+			Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG).show();
 		}
-		
 		//String message = getString(R.string.no_items);
 		//Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+	}
+
+	private boolean isNetworkAvailable() {
+		ConnectivityManager manager = (ConnectivityManager)
+				getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+		
+		boolean isAvailable = false;
+		if(networkInfo != null && networkInfo.isConnected()) {
+			isAvailable = true;
+		}
+		return isAvailable;
 	}
 
 	@Override
@@ -36,6 +58,46 @@ public class MainListActivity extends ListActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_list, menu);
 		return true;
+	}
+	
+	private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+		@Override
+		protected String doInBackground(Object... arg0) {
+			int responseCode = -1;
+			try {
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+				
+				responseCode = connection.getResponseCode();
+				if(responseCode == HttpURLConnection.HTTP_OK){
+					InputStream inputStream = connection.getInputStream();
+					Reader reader = new InputStreamReader(inputStream);
+					int contentLength = connection.getContentLength();
+					char[] charArray = new char[contentLength];
+					reader.read(charArray);
+					String responseData = new String(charArray);
+					Log.v(TAG, responseData);
+				}
+				else{
+					Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+				}
+				Log.i(TAG, "Code: " + responseCode);
+			}
+			catch(MalformedURLException e) {
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			catch(IOException e){
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			catch(Exception e){
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			
+			return "Code: " + responseCode;
+		}
+		
 	}
 
 }
