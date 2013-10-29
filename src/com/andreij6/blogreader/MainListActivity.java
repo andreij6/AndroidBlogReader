23ccc2c2afcd9aeb855d13f1c1146324433d0808
Andre Jones <andreij6@gmail.com>
2013-10-29 15:03:39 -0500

    10/29/03 2or3 vids

--- src/com/andreij6/blogreader/MainListActivity.java
+++ src/com/andreij6/blogreader/MainListActivity.java
@@ -12,6 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -21,7 +22,10 @@ import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
@@ -31,13 +35,17 @@ public class MainListActivity extends ListActivity {
 	public static final int NUMBER_OF_POSTS = 20;
 	public static final String TAG = MainListActivity.class.getSimpleName();
 	protected JSONObject mBlogData;
+	protected ProgressBar mProgressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 		
+		mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+		
 		if(isNetworkAvailable()){
+			mProgressBar.setVisibility(View.VISIBLE);
 			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
 			getBlogPostsTask.execute();
 		}
@@ -67,9 +75,11 @@ public class MainListActivity extends ListActivity {
 		return true;
 	}
 	
-	public void updateList() {
+	public void handleBlogResponse() {
+		mProgressBar.setVisibility(View.INVISIBLE);
+		
 		if(mBlogData == null){
-			//TODO: Handle error
+			updateDisplayForError();
 		}
 		else {
 			try {
@@ -91,6 +101,18 @@ public class MainListActivity extends ListActivity {
 			}
 		}
 		
+	}
+
+	private void updateDisplayForError() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(getString(R.string.error_title));
+		builder.setMessage(getString(R.string.error_message));
+		builder.setPositiveButton(android.R.string.ok, null);
+		AlertDialog dialog = builder.create();
+		dialog.show();
+		
+		TextView emptyTextView = (TextView) getListView().getEmptyView();
+		emptyTextView.setText(getString(R.string.no_items));
 	}
 	
 	private class GetBlogPostsTask extends AsyncTask<Object, Void, JSONObject> {
@@ -137,7 +159,7 @@ public class MainListActivity extends ListActivity {
 		@Override 
 		protected void onPostExecute(JSONObject result){
 			mBlogData = result;
-			updateList();
+			handleBlogResponse();
 		}
 	}
 
